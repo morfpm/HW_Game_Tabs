b52193b040182e80060d720e0393719c17dbd77a
morfpm <morfpm@gmail.com>
2019-05-21 16:26:35 +0300

    with jQuery tabs

--- js/script.js
+++ js/script.js
@@ -1,37 +1,25 @@
 "use strict";
 
-var btns = document.querySelectorAll('.js-btn'),
-    items = document.querySelectorAll('.js-item'),
-    activeClass = 'is-active';
+var btns = $('.js-btn');
+var items = $('.js-item');
+
+var activeClass = 'is-active';
+
+btns.click(function(event){
+    var currentTab = $(this).data('tab');
+
+    btns.removeClass(activeClass);
+    $(this).addClass(activeClass);
+
+    items.hide();
+    $( items[currentTab - 1] ).show();
+});
 
 var rate = +document.querySelector('.cover__rating-list').dataset.rating,
-    rateStars = document.querySelectorAll('.rating-item');
+    rateStars = $('.rating-item');
 
-// ------------ change tab start
-for (var i = 0; i < btns.length; i++) {
-
-    btns[i].addEventListener('click', function(event) {
-        var currentId = this.dataset.tab;
-
-        console.log(currentId);
-
-        for (var j = 0; j < btns.length; j++) {
-            btns[j].classList.remove(activeClass);
-        }
-
-        for (var j = 0; j < items.length; j++) {
-            items[j].classList.remove(activeClass);
-        }
-
-        items[currentId - 1].classList.add(activeClass);
-        this.classList.add(activeClass);
-    });
-}
-// ---------------- change tab END
-// ---------------- process rate start
 if (rate > 5){ rate = 5; }
 
 for (var i = 0; i < rate; i++){
     rateStars[i].classList.add('star-on');
 }
-// ----------------- process rate end
